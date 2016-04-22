cd9598f665fb3e8c8200ebc7ab7e23212ac6a85a
Joaquim Rocha <jrocha@endlessm.com>
2016-04-22 15:23:15 +0200

    Add EOS desktop plugin
    This plugin handles the addition and removal of shortcuts from the EOS
    desktop.
    
    (Rebase 3.32: Fix minor rebase conflicts.)
    
    https://phabricator.endlessm.com/T19983
    https://phabricator.endlessm.com/T19698

---
+++ plugins/eos/gs-plugin-eos.c
@@ -0,0 +1,388 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*-
+ *
+ * Copyright (C) 2016-2018 Endless Mobile, Inc
+ *
+ * Authors:
+ *   Joaquim Rocha <jrocha@endlessm.com>
+ *
+ * Licensed under the GNU General Public License Version 2
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include <config.h>
+
+#include <gnome-software.h>
+#include <glib/gi18n.h>
+#include <gs-plugin.h>
+
+/*
+ * SECTION:
+ * Plugin to improve GNOME Software integration in the EOS desktop.
+ */
+
+struct GsPluginData
+{
+	GDBusConnection *session_bus;
+	GHashTable *desktop_apps;
+	int applications_changed_id;
+};
+
+static gboolean
+get_applications_with_shortcuts (GsPlugin	*plugin,
+				 GHashTable    **apps_table,
+				 GCancellable	*cancellable,
+				 GError		**error) {
+	g_autoptr (GVariantIter) iter = NULL;
+	g_autoptr (GVariant) apps = NULL;
+	gchar *application;
+	GsPluginData *priv = gs_plugin_get_data (plugin);
+
+	*apps_table = g_hash_table_new_full (g_str_hash, g_str_equal, g_free,
+					     NULL);
+
+	apps = g_dbus_connection_call_sync (priv->session_bus,
+					    "org.gnome.Shell",
+					    "/org/gnome/Shell",
+					    "org.gnome.Shell.AppStore",
+					    "ListApplications",
+					    NULL, NULL,
+					    G_DBUS_CALL_FLAGS_NONE,
+					    -1,
+					    cancellable,
+					    error);
+	if (apps == NULL)
+		return FALSE;
+
+	g_variant_get (apps, "(as)", &iter);
+	while (g_variant_iter_loop (iter, "s", &application))
+		g_hash_table_add (*apps_table, g_strdup (application));
+
+	return TRUE;
+}
+
+static void
+on_desktop_apps_changed (GDBusConnection *connection,
+			 const gchar	 *sender_name,
+			 const gchar	 *object_path,
+			 const gchar	 *interface_name,
+			 const gchar	 *signal_name,
+			 GVariant	 *parameters,
+			 GsPlugin	 *plugin)
+{
+	g_autoptr(GHashTable) apps = NULL;
+	GHashTableIter iter;
+	gpointer key, value;
+	GsPluginData *priv = gs_plugin_get_data (plugin);
+	g_autoptr(GError) error = NULL;
+
+	if (!get_applications_with_shortcuts (plugin, &apps, NULL, &error)) {
+		g_warning ("Error getting apps with shortcuts: %s",
+			   error->message);
+		return;
+	}
+
+	/* remove any apps that no longer have shortcuts */
+	g_hash_table_iter_init (&iter, priv->desktop_apps);
+	while (g_hash_table_iter_next (&iter, &key, &value)) {
+		GsApp *app = NULL;
+
+		/* remove the key (if it exists) so we don't have to deal with
+		 * it again in the next loop */
+		if (g_hash_table_remove (apps, key))
+			continue;
+
+		app = gs_plugin_cache_lookup (plugin, key);
+		if (app)
+			gs_app_remove_quirk (app, AS_APP_QUIRK_HAS_SHORTCUT);
+
+		g_hash_table_iter_remove (&iter);
+	}
+
+	/* add any apps that have shortcuts now */
+	g_hash_table_iter_init (&iter, apps);
+	while (g_hash_table_iter_next (&iter, &key, &value)) {
+		GsApp *app = gs_plugin_cache_lookup (plugin, key);
+
+		if (app)
+			gs_app_add_quirk (app, AS_APP_QUIRK_HAS_SHORTCUT);
+
+		g_hash_table_add (priv->desktop_apps, g_strdup (key));
+	}
+}
+
+gboolean
+gs_plugin_setup (GsPlugin *plugin,
+		 GCancellable *cancellable,
+		 GError **error)
+{
+	g_autoptr(GError) local_error = NULL;
+	GApplication *app = g_application_get_default ();
+	GsPluginData *priv = gs_plugin_get_data (plugin);
+
+	priv->session_bus = g_application_get_dbus_connection (app);
+
+	if (!get_applications_with_shortcuts (plugin, &priv->desktop_apps,
+					      cancellable, &local_error))
+		g_warning ("Couldn't get the apps with shortcuts: %s",
+			   local_error->message);
+
+	priv->applications_changed_id =
+		g_dbus_connection_signal_subscribe (priv->session_bus,
+						    "org.gnome.Shell",
+						    "org.gnome.Shell.AppStore",
+						    "ApplicationsChanged",
+						    "/org/gnome/Shell",
+						    NULL,
+						    G_DBUS_SIGNAL_FLAGS_NONE,
+						    (GDBusSignalCallback) on_desktop_apps_changed,
+						    plugin, NULL);
+	return TRUE;
+}
+
+void
+gs_plugin_initialize (GsPlugin *plugin)
+{
+	gs_plugin_alloc_data (plugin, sizeof(GsPluginData));
+
+	/* let the flatpak plugin run first so we deal with the apps
+	 * in a more complete/refined state */
+	gs_plugin_add_rule (plugin, GS_PLUGIN_RULE_RUN_AFTER, "flatpak");
+}
+
+void
+gs_plugin_destroy (GsPlugin *plugin)
+{
+	GsPluginData *priv = gs_plugin_get_data (plugin);
+
+	if (priv->applications_changed_id != 0) {
+		g_dbus_connection_signal_unsubscribe (priv->session_bus,
+						      priv->applications_changed_id);
+		priv->applications_changed_id = 0;
+	}
+
+	g_hash_table_destroy (priv->desktop_apps);
+}
+
+static void
+gs_plugin_eos_update_app_shortcuts_info (GsPlugin *plugin,
+					 GsApp *app)
+{
+	GsPluginData *priv = NULL;
+	const char *desktop_file_id = NULL;
+	g_autofree char *kde_desktop_file_id = NULL;
+
+	if (!gs_app_is_installed (app)) {
+		gs_app_remove_quirk (app, AS_APP_QUIRK_HAS_SHORTCUT);
+		return;
+	}
+
+	priv = gs_plugin_get_data (plugin);
+	desktop_file_id = gs_app_get_id (app);
+	kde_desktop_file_id =
+		g_strdup_printf ("%s-%s", "kde4", desktop_file_id);
+
+	/* Cache both keys, since we may see either variant in the desktop
+	 * grid; see on_desktop_apps_changed().
+	 */
+	gs_plugin_cache_add (plugin, desktop_file_id, app);
+	gs_plugin_cache_add (plugin, kde_desktop_file_id, app);
+
+	if (g_hash_table_lookup (priv->desktop_apps, desktop_file_id) ||
+	    g_hash_table_lookup (priv->desktop_apps, kde_desktop_file_id))
+		gs_app_add_quirk (app, AS_APP_QUIRK_HAS_SHORTCUT);
+	else
+		gs_app_remove_quirk (app, AS_APP_QUIRK_HAS_SHORTCUT);
+}
+
+static gboolean
+app_is_flatpak (GsApp *app)
+{
+	return gs_app_get_bundle_kind (app) == AS_BUNDLE_KIND_FLATPAK;
+}
+
+void
+gs_plugin_adopt_app (GsPlugin *plugin, GsApp *app)
+{
+	if (app_is_flatpak (app))
+		return;
+
+	gs_app_set_management_plugin (app, gs_plugin_get_name (plugin));
+}
+
+gboolean
+gs_plugin_refine (GsPlugin		*plugin,
+		  GsAppList		*list,
+		  GsPluginRefineFlags	flags,
+		  GCancellable		*cancellable,
+		  GError		**error)
+{
+	for (guint i = 0; i < gs_app_list_length (list); ++i) {
+		GsApp *app = gs_app_list_index (list, i);
+
+		if (gs_app_get_kind (app) != AS_APP_KIND_DESKTOP)
+			continue;
+
+		gs_plugin_eos_update_app_shortcuts_info (plugin, app);
+	}
+
+	return TRUE;
+}
+
+static gboolean
+remove_app_from_shell (GsPlugin		*plugin,
+		       GsApp		*app,
+		       GCancellable	*cancellable,
+		       GError		**error_out)
+{
+	GError *error = NULL;
+	GsPluginData *priv = gs_plugin_get_data (plugin);
+	const char *desktop_file_id = gs_app_get_id (app);
+	g_autoptr(GDesktopAppInfo) app_info =
+		gs_utils_get_desktop_app_info (desktop_file_id);
+	const char *shortcut_id = g_app_info_get_id (G_APP_INFO (app_info));
+
+	g_dbus_connection_call_sync (priv->session_bus,
+				     "org.gnome.Shell",
+				     "/org/gnome/Shell",
+				     "org.gnome.Shell.AppStore",
+				     "RemoveApplication",
+				     g_variant_new ("(s)", shortcut_id),
+				     NULL,
+				     G_DBUS_CALL_FLAGS_NONE,
+				     -1,
+				     cancellable,
+				     &error);
+
+	if (error != NULL) {
+		g_debug ("Error removing app from shell: %s", error->message);
+		g_propagate_error (error_out, error);
+		return FALSE;
+	}
+
+	return TRUE;
+}
+
+static gboolean
+add_app_to_shell (GsPlugin	*plugin,
+		  GsApp		*app,
+		  GCancellable	*cancellable,
+		  GError	**error_out)
+{
+	GError *error = NULL;
+	GsPluginData *priv = gs_plugin_get_data (plugin);
+	const char *desktop_file_id = gs_app_get_id (app);
+	g_autoptr(GDesktopAppInfo) app_info =
+		gs_utils_get_desktop_app_info (desktop_file_id);
+	const char *shortcut_id = g_app_info_get_id (G_APP_INFO (app_info));
+
+	g_dbus_connection_call_sync (priv->session_bus,
+				     "org.gnome.Shell",
+				     "/org/gnome/Shell",
+				     "org.gnome.Shell.AppStore",
+				     "AddAppIfNotVisible",
+				     g_variant_new ("(s)", shortcut_id),
+				     NULL,
+				     G_DBUS_CALL_FLAGS_NONE,
+				     -1,
+				     cancellable,
+				     &error);
+
+	if (error != NULL) {
+		g_debug ("Error adding app to shell: %s", error->message);
+		g_propagate_error (error_out, error);
+		return FALSE;
+	}
+
+	return TRUE;
+}
+
+gboolean
+gs_plugin_add_shortcut (GsPlugin	*plugin,
+			GsApp		*app,
+			GCancellable	*cancellable,
+			GError		**error)
+{
+	gs_app_add_quirk (app, AS_APP_QUIRK_HAS_SHORTCUT);
+	return add_app_to_shell (plugin, app, cancellable, error);
+}
+
+gboolean
+gs_plugin_remove_shortcut (GsPlugin	*plugin,
+			   GsApp	*app,
+			   GCancellable	*cancellable,
+			   GError	**error)
+{
+	gs_app_remove_quirk (app, AS_APP_QUIRK_HAS_SHORTCUT);
+	return remove_app_from_shell (plugin, app, cancellable, error);
+}
+
+gboolean
+gs_plugin_app_install (GsPlugin *plugin,
+		       GsApp *app,
+		       GCancellable *cancellable,
+		       GError **error)
+{
+	g_autoptr(GError) local_error = NULL;
+	if (!app_is_flatpak (app))
+		return TRUE;
+
+	/* We're only interested in already installed flatpak apps so we can
+	 * add them to the desktop */
+	if (gs_app_get_state (app) != AS_APP_STATE_INSTALLED)
+		return TRUE;
+
+	if (!add_app_to_shell (plugin, app, cancellable, &local_error)) {
+		g_warning ("Failed to add shortcut: %s",
+			   local_error->message);
+	}
+	return TRUE;
+}
+
+gboolean
+gs_plugin_app_remove (GsPlugin *plugin,
+		      GsApp *app,
+		      GCancellable *cancellable,
+		      GError **error)
+{
+	g_autoptr(GError) local_error = NULL;
+	if (!app_is_flatpak (app))
+		return TRUE;
+
+	/* We're only interested in apps that have been successfully uninstalled */
+	if (gs_app_is_installed (app))
+		return TRUE;
+
+	if (!remove_app_from_shell (plugin, app, cancellable, &local_error)) {
+		g_warning ("Failed to remove shortcut: %s",
+			   local_error->message);
+	}
+	return TRUE;
+}
+
+gboolean
+gs_plugin_launch (GsPlugin *plugin,
+		  GsApp *app,
+		  GCancellable *cancellable,
+		  GError **error)
+{
+	/* if the app is one of the system ones, we simply launch it through the
+	 * plugin's app launcher */
+	if (gs_app_has_quirk (app, AS_APP_QUIRK_COMPULSORY) &&
+	    !app_is_flatpak (app))
+		return gs_plugin_app_launch (plugin, app, error);
+
+	return TRUE;
+}
