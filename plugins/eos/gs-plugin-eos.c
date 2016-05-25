db51c180d1494f6167b73b66a6444a1bf817e6d4
Joaquim Rocha <jrocha@endlessm.com>
2016-05-25 13:48:17 +0200

    eos: Blacklist web link applications
    Web links are managed by a different application in EOS.
    
    https://phabricator.endlessm.com/T11685

--- plugins/eos/gs-plugin-eos.c
+++ plugins/eos/gs-plugin-eos.c
@@ -165,6 +165,17 @@ get_applications_with_shortcuts (GsPlugin	*plugin,
 	return apps_table;
 }
 
+static gboolean
+gs_plugin_eos_blacklist_if_needed (GsApp *app)
+{
+	if (g_str_has_prefix (gs_app_get_id (app), "eos-link-")) {
+		gs_app_add_category (app, "Blacklisted");
+		return TRUE;
+	}
+
+	return FALSE;
+}
+
 static void
 gs_plugin_eos_update_app_shortcuts_info (GsPlugin *plugin,
 					 GsApp *app,
@@ -244,6 +255,9 @@ gs_plugin_refine (GsPlugin		*plugin,
 		GsApp *app = gs_app_list_index (list, i);
 
 		if (gs_app_get_kind (app) != AS_APP_KIND_DESKTOP)
+			continue;
+
+		if (gs_plugin_eos_blacklist_if_needed (app))
 			continue;
 
 		gs_plugin_eos_update_app_shortcuts_info (plugin, app, apps);
