c6ad69e9b9722ef101ac7a54d222f5334b0f2d84
Joaquim Rocha <jrocha@endlessm.com>
2016-05-25 13:48:17 +0200

    eos: Blacklist web link applications
    Web links are managed by a different application in EOS.
    
    (Rebase 3.32: Drop most of the commit as it’s been moved to the
    `eos-blacklist` plugin as part of T26511. The remainder is unrelated to
    blacklisting.)
    
    https://phabricator.endlessm.com/T11685

--- plugins/eos/gs-plugin-eos.c
+++ plugins/eos/gs-plugin-eos.c
@@ -222,6 +222,14 @@ gs_plugin_adopt_app (GsPlugin *plugin, GsApp *app)
 	gs_app_set_management_plugin (app, gs_plugin_get_name (plugin));
 }
 
+static void
+gs_plugin_eos_refine_core_app (GsApp *app)
+{
+	/* we only allow to remove flatpak apps */
+	if (!app_is_flatpak (app))
+		gs_app_add_quirk (app, AS_APP_QUIRK_COMPULSORY);
+}
+
 gboolean
 gs_plugin_refine (GsPlugin		*plugin,
 		  GsAppList		*list,
@@ -231,6 +239,8 @@ gs_plugin_refine (GsPlugin		*plugin,
 {
 	for (guint i = 0; i < gs_app_list_length (list); ++i) {
 		GsApp *app = gs_app_list_index (list, i);
+
+		gs_plugin_eos_refine_core_app (app);
 
 		if (gs_app_get_kind (app) != AS_APP_KIND_DESKTOP)
 			continue;
