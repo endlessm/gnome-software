53253d55a7295b38c4387fb66b416f733d1e8244
Joaquim Rocha <jrocha@endlessm.com>
2016-06-09 15:56:44 +0200

    Add a method to gs-flatpak to know if a GsApp is already installed
    This will be useful for the external apps plugin.

--- src/plugins/gs-flatpak.c
+++ src/plugins/gs-flatpak.c
@@ -1642,3 +1642,31 @@ gs_flatpak_get_prefix (GsFlatpak *self)
 
 	return NULL;
 }
+
+gboolean
+gs_flatpak_is_installed (GsFlatpak *self,
+			 GsApp *app,
+			 GCancellable *cancellable,
+			 GError **error)
+{
+	g_autoptr(FlatpakInstalledRef) ref;
+	FlatpakRefKind kind;
+	const char *name;
+	const char *arch;
+	const char *branch;
+
+	kind = gs_app_get_flatpak_kind (app);
+	name = gs_app_get_flatpak_name (app);
+	arch = gs_app_get_flatpak_arch (app);
+	branch = gs_app_get_flatpak_branch (app);
+
+	ref = flatpak_installation_get_installed_ref (self->installation,
+						      kind,
+						      name,
+						      arch,
+						      branch,
+						      cancellable,
+						      error);
+
+	return ref != NULL;
+}
