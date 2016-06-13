5bae127685e4b3b0974f702e7107edbe19bbdfda
Joaquim Rocha <jrocha@endlessm.com>
2016-06-13 15:58:11 +0200

    Add plugins for cleaning up the legacy external runtimes
    The plugin "legacy-external-apps" gets the info about the external
    runtime from the installed AppStream data and runs before the "flatpak"
    plugin so we can get this info before the app is changed
    (removed/updated). That info is set as a metadata item in the GsApp
    object.
    
    The plugin "external-runtimes-cleaner" uses the info mentioned above
    to remove the external runtimes once an app is updated or removed. It
    runs after the Flatpak plugin and ensures that such runtimes are only
    removed if the app has been successfully updated/removed.
    
    https://phabricator.endlessm.com/T14442

---
+++ plugins/eos/gs-legacy-external-apps.h
@@ -0,0 +1,29 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*-
+ *
+ * Copyright (C) 2016 Endless Mobile, Inc
+ *
+ * Author: Joaquim Rocha <jrocha@endlessm.com>
+ *
+ * Licensed under the GNU General Public License Version 2
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef __GS_LEGACY_EXTERNAL_APPS_H__
+#define __GS_LEGACY_EXTERNAL_APPS_H__
+
+#define LEGACY_RUNTIME_INSTALLED_MTD_KEY "EndlessOS::legacy-ext-runtime-installed"
+
+#endif /* __GS_LEGACY_EXTERNAL_APPS_H__ */
