e605f5e7e2c95b4499c7ad9860d7bf7d3cf2adb0
Joaquim Rocha <jrocha@endlessm.com>
2016-06-16 21:58:07 +0200

    Add a side filter list to the shell
    This list will be used to select the page that should be displayed.
    
    (Rebase 3.32: Fix trivial merge conflicts.)
    
    https://phabricator.endlessm.com/T16949
    https://phabricator.endlessm.com/T19877

--- src/gs-shell.h
+++ src/gs-shell.h
@@ -87,5 +87,10 @@ void		 gs_shell_setup			(GsShell	*shell,
 						 GCancellable	*cancellable);
 gboolean	 gs_shell_is_active		(GsShell	*shell);
 GtkWindow	*gs_shell_get_window		(GsShell	*shell);
+void		 gs_shell_side_filter_set_visible	(GsShell	*shell,
+							 gboolean	visible);
+GtkWidget	*gs_shell_side_filter_add_category	(GsShell	*shell,
+							 GsCategory	*cat);
+void		 gs_shell_side_filter_clear_categories	(GsShell	*shell);
 
 G_END_DECLS
