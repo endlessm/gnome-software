08fc40153e40d566ae0f9e6b5aa06e537d4fe040
Joaquim Rocha <jrocha@endlessm.com>
2016-06-20 10:59:52 +0200

    Keep the top tabs visible when in the category page
    Those are kept visible instead of replacing them with a label because
    we have the side filter to show the category name already.

--- src/gs-shell.c
+++ src/gs-shell.c
@@ -2306,7 +2306,6 @@ gs_shell_show_app (GsShell *shell, GsApp *app)
 void
 gs_shell_show_category (GsShell *shell, GsCategory *category)
 {
-	save_back_entry (shell);
 	gs_shell_change_mode (shell, GS_SHELL_MODE_CATEGORY, category, TRUE);
 }
 
