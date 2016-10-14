0256712bb6c126393880cebfbb819247c4605083
Joaquim Rocha <jrocha@endlessm.com>
2016-10-15 00:01:07 +0200

    Do not set a cache age on manual refresh
    We need things to always happen immediately when pressing the refresh
    button and not only if the cache is older than 10 minutes.
    
    https://phabricator.endlessm.com/T12998

--- src/gs-shell-updates.c
+++ src/gs-shell-updates.c
@@ -769,7 +769,7 @@ gs_shell_updates_get_new_updates (GsShellUpdates *self)
 	if (g_settings_get_boolean (self->settings, "download-updates"))
 		refresh_flags |= GS_PLUGIN_REFRESH_FLAGS_PAYLOAD;
 	gs_plugin_loader_refresh_async (self->plugin_loader,
-					10 * 60,
+					0,
 					refresh_flags,
 					GS_PLUGIN_FAILURE_FLAGS_USE_EVENTS,
 					self->cancellable_refresh,
