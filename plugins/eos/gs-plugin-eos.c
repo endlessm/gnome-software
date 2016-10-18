f63e654d2252e9e596e07e32e0668835dc990e1f
Roddy Shuler <roddy@endlessm.com>
2016-10-18 10:41:02 -0700

    eos: Block some apps in Chinese (zh_CN) images
    Excludes:
      * All Encyclopedia apps;
      * Translate (which uses Google Translate);
      * VideoNet (which plays YouTube videos);
      * Google Chrome;
    
    https://phabricator.endlessm.com/T13291
    https://phabricator.endlessm.com/T14259

--- plugins/eos/gs-plugin-eos.c
+++ plugins/eos/gs-plugin-eos.c
@@ -432,8 +432,13 @@ app_is_banned_for_personality (GsPlugin *plugin, GsApp *app)
 	if (gs_app_is_installed (app))
 		return FALSE;
 
-	return (g_strcmp0 (priv->personality, "es_GT") == 0) &&
-		(g_strcmp0 (id, "org.openarena.Openarena.desktop") == 0);
+	return ((g_strcmp0 (priv->personality, "es_GT") == 0) &&
+	        (g_strcmp0 (id, "org.openarena.Openarena.desktop") == 0)) ||
+	       ((g_strcmp0 (priv->personality, "zh_CN") == 0) &&
+	        ((g_strcmp0 (id, "com.google.Chrome.desktop") == 0) ||
+	         (g_strcmp0 (id, "com.endlessm.translation.desktop") == 0) ||
+	         (g_strcmp0 (id, "com.endlessm.videonet.desktop") == 0) ||
+	         g_str_has_prefix (id, "com.endlessm.encyclopedia")));
 }
 
 static gboolean
