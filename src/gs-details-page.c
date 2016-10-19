3eff4e478b0bce949c8436ef1f0318b3f62aa941
Joaquim Rocha <jrocha@endlessm.com>
2016-10-19 20:59:11 +0200

    Emphasize the "Add to desktop" button
    It is now the first button in the row and the one always highlighted.
    
    (Rebase 3.32: Fix significant merge conflicts. And again for second
    pass.)
    
    https://phabricator.endlessm.com/T13763
    https://phabricator.endlessm.com/T14949
    https://phabricator.endlessm.com/T19696
    https://phabricator.endlessm.com/T19764

--- src/gs-details-page.c
+++ src/gs-details-page.c
@@ -193,12 +193,14 @@ gs_details_page_update_shortcut_button (GsDetailsPage *self)
 	gboolean add_shortcut_func;
 	gboolean remove_shortcut_func;
 	gboolean has_shortcut;
+	GtkStyleContext *style_context = gtk_widget_get_style_context (self->button_install);
 
 	gtk_widget_set_visible (self->button_details_add_shortcut,
 				FALSE);
 	gtk_widget_set_visible (self->button_details_remove_shortcut,
 				FALSE);
 
+	gtk_style_context_add_class (style_context, "suggested-action");
 	if (gs_app_get_kind (self->app) != AS_APP_KIND_DESKTOP)
 		return;
 
@@ -234,6 +236,11 @@ gs_details_page_update_shortcut_button (GsDetailsPage *self)
 		gtk_widget_set_sensitive (self->button_details_remove_shortcut,
 					  has_shortcut);
 	}
+
+	if (gtk_widget_get_visible (self->button_details_add_shortcut))
+		gtk_style_context_remove_class (style_context, "suggested-action");
+	else
+		gtk_style_context_add_class (style_context, "suggested-action");
 }
 
 static gboolean
