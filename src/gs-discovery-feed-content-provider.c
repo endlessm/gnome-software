a00b12040a8a6b21d737ee320189f0993125c624
Sam Spilsbury <sam@endlessm.com>
2017-05-28 11:24:47 +0800

    feed: Add missing space

--- src/gs-discovery-feed-content-provider.c
+++ src/gs-discovery-feed-content-provider.c
@@ -119,7 +119,7 @@ get_app_thumbnail_cached_filename (GsApp *app)
 	 * since it will have a thumbnail */
 	if (g_file_test (cache_filename, G_FILE_TEST_EXISTS)) {
 		g_debug ("Hit cache for thumbnail when loading discovery feed %s: %s", popular_background_url, cache_filename);
-		return g_steal_pointer(&cache_filename);
+		return g_steal_pointer (&cache_filename);
 	}
 
 	return NULL;
