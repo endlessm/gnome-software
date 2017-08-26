8ea44fba21599e60ff3b9bef401eb9fda5b21913
Sam Spilsbury <sam@endlessm.com>
2017-08-27 03:59:27 +0800

    lib: Split refine helpers into a separate file
    We'll want to use this from gnome-software itself

--- lib/gs-refine.h
+++ lib/gs-refine.h
@@ -1,6 +1,6 @@
 /* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*-
  *
- * Copyright (C) 2016 Richard Hughes <richard@hughsie.com>
+ * Copyright (C) 2013-2016 Richard Hughes <richard@hughsie.com>
  *
  * Licensed under the GNU General Public License Version 2
  *
@@ -19,22 +19,15 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-#ifndef __GNOME_SOFTWARE_H__
-#define __GNOME_SOFTWARE_H__
+#ifndef __GS_REFINE_H
+#define __GS_REFINE_H
 
-#ifndef I_KNOW_THE_GNOME_SOFTWARE_API_IS_SUBJECT_TO_CHANGE
-#error You have to define I_KNOW_THE_GNOME_SOFTWARE_API_IS_SUBJECT_TO_CHANGE
+#include <glib-2.0/glib.h>
+
+G_BEGIN_DECLS
+
+guint64 gs_parse_refine_flags (const gchar *extra, GError **error);
+
+G_END_DECLS
+
 #endif
-
-#include <gs-app.h>
-#include <gs-app-list.h>
-#include <gs-auth.h>
-#include <gs-category.h>
-#include <gs-os-release.h>
-#include <gs-plugin.h>
-#include <gs-plugin-vfuncs.h>
-#include <gs-refine.h>
-#include <gs-utils.h>
-
-#endif /* __GNOME_SOFTWARE_H__ */
-
