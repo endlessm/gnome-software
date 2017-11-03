0b898bc2a8e9ca9a7c81001f403fe3a0c566ca6a
Philip Chimento <philip@endlessm.com>
2017-11-02 17:41:57 -0700

    flatpak: Associate corresponding EknServices with runtime
    This computes the EknServices ref that corresponds to any particular
    runtime that is to be installed, and also queues it up for install if
    it's not installed already.
    
    It keeps using com.endlessm.EknServices2 until we bump the version
    number of that service due to some backwards-incompatibility.
    
    https://phabricator.endlessm.com/T19806
    https://phabricator.endlessm.com/T20846

--- plugins/flatpak/gs-flatpak.c
+++ plugins/flatpak/gs-flatpak.c
@@ -2505,6 +2505,70 @@ gs_flatpak_app_remove_source (GsFlatpak *self,
 	return TRUE;
 }
 
+/*
+ * gs_flatpak_get_services_app_for_runtime:
+ * Specific to Endless runtimes, gets a GsApp reference for the related
+ * "EknServices" app that provides system services for apps that use the
+ * given runtime.
+ *
+ * Returns NULL if no services app was required or there was an error.
+ * Does not take a GError since any error should be recoverable. However, in
+ * order to make sure the operation was not cancelled, you should check the
+ * status of the GCancellable after calling this.
+ */
+static GsApp *
+gs_flatpak_get_services_app_for_runtime (GsFlatpak *self, GsApp *runtime,
+                                         GCancellable *cancellable)
+{
+	const gchar *runtime_id;
+	const gchar *runtime_branch;
+	const gchar *services_branch;
+	const gchar *arch;
+	g_autofree gchar *services_id = NULL;
+	g_autofree gchar *description = NULL;
+	g_autoptr(FlatpakRef) services_ref = NULL;
+	g_autoptr(GError) error = NULL;
+	g_autoptr(GsApp) services_app = NULL;
+
+	runtime_id = gs_app_get_id (runtime);
+	runtime_branch = gs_app_get_branch (runtime);
+
+	if ((g_strcmp0 (runtime_id, "com.endlessm.Platform") == 0 &&
+	     g_strcmp0 (runtime_branch, "eos3.1") == 0) ||
+	    (g_strcmp0 (runtime_id, "com.endlessm.apps.Platform") == 0 &&
+	     g_strcmp0 (runtime_branch, "1") == 0)) {
+		services_id = g_strdup ("com.endlessm.EknServices");
+		services_branch = "eos3";
+	} else if (g_strcmp0 (runtime_id, "com.endlessm.apps.Platform") == 0) {
+		services_id = g_strdup ("com.endlessm.EknServices2");
+		if (g_strcmp0 (runtime_branch, "master") == 0)
+			services_branch = "master";
+		else
+			services_branch = "stable";
+	} else {
+		/* Runtime doesn't require an EknServices app */
+		return NULL;
+	}
+
+	/* Construct a GsApp for the EknServices we determined we needed */
+	arch = gs_flatpak_app_get_ref_arch (runtime);
+	description = g_strdup_printf ("app/%s/%s/%s", services_id, arch, services_branch);
+	services_ref = flatpak_ref_parse (description, &error);
+	if (services_ref == NULL) {
+		g_critical ("Invalid flatpak ref description %s: %s", description,
+		            error->message);
+		return NULL;
+	}
+
+	services_app = gs_flatpak_create_app (self, services_ref);
+	if (!gs_plugin_refine_item_origin (self, services_app, cancellable, &error)) {
+		g_warning ("Couldn't discover origin for %s: %s", description,
+		           error->message);
+		return NULL;
+	}
+	return g_steal_pointer (&services_app);
+}
+
 static GsAppList *
 gs_flatpak_get_list_for_remove (GsFlatpak *self, GsApp *app,
 				GCancellable *cancellable, GError **error)
@@ -2658,6 +2722,23 @@ gs_flatpak_refine_runtime_for_install (GsFlatpak *self,
 	return TRUE;
 }
 
+static void
+gs_flatpak_add_app_to_list_if_not_installed (GsApp *app, GsAppList *list,
+					     const GHashTable *hash_installed)
+{
+	g_autofree gchar *ref_display = gs_flatpak_app_get_ref_display (app);
+	if (g_hash_table_contains (hash_installed, ref_display)) {
+		g_debug ("%s is already installed, so skipping",
+			 gs_app_get_id (app));
+		return;
+	}
+
+	g_debug ("%s/%s is not already installed, so installing",
+		 gs_flatpak_app_get_ref_name (app),
+		 gs_flatpak_app_get_ref_branch (app));
+	gs_app_list_add (list, app);
+}
+
 static GsAppList *
 gs_flatpak_get_list_for_install_or_update (GsFlatpak *self,
 					   GsApp *app,
@@ -2671,6 +2752,8 @@ gs_flatpak_get_list_for_install_or_update (GsFlatpak *self,
 	g_autoptr(GPtrArray) xrefs_installed = NULL;
 	g_autoptr(GHashTable) hash_installed = NULL;
 	g_autoptr(GsAppList) list = gs_app_list_new ();
+	g_autoptr(GsApp) services_app = NULL;
+	g_autoptr(GsApp) services_runtime = NULL;
 
 	/* get the list of installed apps */
 	xrefs_installed = flatpak_installation_list_installed_refs (self->installation,
@@ -2691,18 +2774,23 @@ gs_flatpak_get_list_for_install_or_update (GsFlatpak *self,
 	if (!gs_flatpak_refine_runtime_for_install (self, app, cancellable, error))
 		return FALSE;
 	runtime = gs_app_get_update_runtime (app);
-	if (runtime != NULL) {
-		g_autofree gchar *ref_display = NULL;
-		ref_display = gs_flatpak_app_get_ref_display (runtime);
-		if (g_hash_table_contains (hash_installed, ref_display)) {
-			g_debug ("%s is already installed, so skipping",
-				 gs_app_get_id (runtime));
-		} else {
-			g_debug ("%s/%s is not already installed, so installing",
-				 gs_flatpak_app_get_ref_name (runtime),
-				 gs_flatpak_app_get_ref_branch (runtime));
-			gs_app_list_add (list, runtime);
-		}
+	if (runtime != NULL)
+		gs_flatpak_add_app_to_list_if_not_installed (runtime, list, hash_installed);
+
+	/* add services flatpak */
+	services_app = gs_flatpak_get_services_app_for_runtime (self, runtime, cancellable);
+	if (g_cancellable_set_error_if_cancelled (cancellable, error))
+		return FALSE;
+	if (services_app != NULL) {
+		gs_flatpak_add_app_to_list_if_not_installed (services_app, list, hash_installed);
+
+		/* add services flatpak's runtime, if different from app's runtime */
+		if (!gs_flatpak_refine_runtime_for_install (self, services_app,
+							    cancellable, error))
+			return FALSE;
+		services_runtime = gs_app_get_update_runtime (services_app);
+		if (services_runtime != NULL)
+			gs_flatpak_add_app_to_list_if_not_installed (services_runtime, list, hash_installed);
 	}
 
 	/* lookup any related refs for this ref */
