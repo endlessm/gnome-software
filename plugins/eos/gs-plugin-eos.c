b850c943711eb12a43819bb8bb886c6632562947
Robert McQueen <rob@endlessm.com>
2018-04-12 11:15:07 +0100

    eos: Include EknServicesMultiplexer in proxy app
    The versioned EknServices apps have been deprecated and replaced by
    a single multiplexer app.

--- plugins/eos/gs-plugin-eos.c
+++ plugins/eos/gs-plugin-eos.c
@@ -752,9 +752,8 @@ add_updates (GsPlugin *plugin,
 	g_autoptr(GSList) proxied_updates = NULL;
 	const char *proxied_apps[] = {"com.endlessm.Platform",
 				      "com.endlessm.apps.Platform",
-				      "com.endlessm.EknServices.desktop",
-				      "com.endlessm.EknServices2.desktop",
 				      "com.endlessm.CompanionAppService.desktop",
+				      "com.endlessm.EknServicesMultiplexer.desktop",
 				      "com.endlessm.quote_of_the_day.en.desktop",
 				      "com.endlessm.word_of_the_day.en.desktop",
 				      NULL};
