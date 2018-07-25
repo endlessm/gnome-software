23977508fd0bd43750e3d60144bc1fdeb55d9656
Umang Jain <umang@endlessm.com>
2018-07-25 19:40:49 +0530

    Restore window activation while showing a local file
    Fallout from commit 54863ceaf54cf5128f19b7f6e4338905a1b155d0
    
    https://phabricator.endlessm.com/T23365

--- src/gs-shell.c
+++ src/gs-shell.c
@@ -2141,6 +2141,7 @@ gs_shell_show_local_file (GsShell *shell, GFile *file)
 	gs_app_set_local_file (app, file);
 	gs_shell_change_mode (shell, GS_SHELL_MODE_DETAILS,
 			      app, TRUE);
+	gs_shell_activate (shell);
 }
 
 void

endlessm/gnome-software

commit 54863ceaf54cf5128f19b7f6e4338905a1b155d0

flatpak: Select USB category when loading a folder that has a removable remote
When a directory is loaded and appears to contain an ostree repo, we
create a source GsApp that has a special metadata key that indicates
that the categories must be reloaded. This will show the USB category if
any app has it, in order to tell the user which apps are coming from the
USB drives they've inserted.

https://phabricator.endlessm.com/T16949

src/gs-shell.c
@@ -95,10 +95,16 @@ enum {
 
 static guint signals [SIGNAL_LAST] = { 0 };
 
+typedef struct {
+	GsShell *shell;
+	gchar *category_name;
+} CategoriesLoadedData;
+
 static void gs_shell_side_filter_add_mode (GsShell *shell, GsShellMode mode);
 static void side_filter_select_by_mode (GsShell *shell, GsShellMode mode);
 static void side_filter_row_selected (GtkListBox *side_filter,
 				      GsSideFilterRow *row, gpointer data);
+static GsCategory *side_filter_get_category_by_name (GsShell *shell, const gchar *category_id);
 
 static void
 modal_dialog_unmapped_cb (GtkWidget *dialog,
@@ -580,6 +586,43 @@ initial_overview_load_done (GsOverviewPage *overview_page, gpointer data)
 	gs_page_reload (page);
 
 	g_signal_emit (shell, signals[SIGNAL_LOADED], 0);
+}
+
+static void
+on_overview_categories_loaded (GsOverviewPage *page, gpointer user_data)
+{
+	g_autofree CategoriesLoadedData *data = (CategoriesLoadedData *) user_data;
+	GsCategory *category = side_filter_get_category_by_name (data->shell,
+								 data->category_name);
+
+	g_signal_handlers_disconnect_by_func (page, on_overview_categories_loaded,
+					      user_data);
+
+	if (category == NULL) {
+		g_warning ("No %s category after overview categories are refreshed!",
+			   data->category_name);
+		gs_shell_change_mode (data->shell, GS_SHELL_MODE_OVERVIEW, NULL, TRUE);
+		return;
+	}
+
+	gs_shell_change_mode (data->shell, GS_SHELL_MODE_CATEGORY, (gpointer) category,
+			      TRUE);
+}
+
+static void
+reload_overview_and_select_category (GsShell *shell, const gchar *category_name)
+{
+	GsPage *page;
+	GsShellPrivate *priv = gs_shell_get_instance_private (shell);
+	CategoriesLoadedData *data = g_new (CategoriesLoadedData, 1);
+
+	data->shell = shell;
+	data->category_name = g_strdup (category_name);
+	page = GS_PAGE (g_hash_table_lookup (priv->pages, "overview"));
+
+	g_signal_connect (page, "categories-loaded",
+			  G_CALLBACK (on_overview_categories_loaded), data);
+	gs_page_reload (page);
 }
 
 static gboolean
@@ -2008,15 +2051,96 @@ gs_shell_show_search (GsShell *shell, const gchar *search)
 			      (gpointer) search, TRUE);
 }
 
+static void
+gs_shell_file_to_app_cb (GObject *source,
+			 GAsyncResult *res,
+			 gpointer user_data)
+{
+	GsPluginLoader *plugin_loader = GS_PLUGIN_LOADER (source);
+	GsShell *shell = GS_SHELL (user_data);
+	g_autoptr(GError) error = NULL;
+	g_autofree gchar *tmp = NULL;
+	const gchar *removable_media_cat = NULL;
+	g_autoptr(GsAppList) list = NULL;
+	GsApp *app;
+
+	list = gs_plugin_loader_job_process_finish (plugin_loader,
+						    res,
+						    &error);
+	if (list == NULL) {
+		g_warning ("failed to convert file to GsApp: %s", error->message);
+		/* go back to the overview */
+		gs_shell_change_mode (shell, GS_SHELL_MODE_OVERVIEW, NULL, FALSE);
+		return;
+	} else {
+		app = gs_app_list_index (list, 0);
+	}
+
+	/* print what we've got */
+	tmp = gs_app_to_string (app);
+	g_debug ("App from file:\n%s", tmp);
+
+	removable_media_cat = gs_app_get_metadata_item (app, "EndlessOS::RemovableMediaCategory");
+	if (removable_media_cat != NULL) {
+		GsCategory *category;
+		if (gs_app_get_metadata_item (app, "EndlessOS::ReloadOverview") == NULL) {
+			reload_overview_and_select_category (shell, removable_media_cat);
+		} else {
+			category = side_filter_get_category_by_name (shell, removable_media_cat);
+			if (category == NULL)
+				reload_overview_and_select_category (shell, removable_media_cat);
+			else {
+				gs_shell_change_mode (shell, GS_SHELL_MODE_CATEGORY,
+						      (gpointer) category, TRUE);
+			}
+		}
+		gs_shell_activate (shell);
+		return;
+	}
+
+	save_back_entry (shell);
+	/* change widgets */
+	gs_shell_change_mode (shell, GS_SHELL_MODE_DETAILS,
+			      app, TRUE);
+}
+
 void
 gs_shell_show_local_file (GsShell *shell, GFile *file)
 {
 	g_autoptr(GsApp) app = gs_app_new (NULL);
-	save_back_entry (shell);
+	g_autoptr(GsPluginJob) plugin_job = NULL;
+	GsShellPrivate *priv = gs_shell_get_instance_private (shell);
+	GFileType file_type = g_file_query_file_type (file, G_FILE_QUERY_INFO_NONE,
+						      NULL);
+
+	/* check if this is rather a directory */
+	if (file_type == G_FILE_TYPE_DIRECTORY) {
+		plugin_job = gs_plugin_job_newv (GS_PLUGIN_ACTION_FILE_TO_APP,
+						 "file", file,
+						 "failure-flags", GS_PLUGIN_FAILURE_FLAGS_USE_EVENTS,
+						 "refine-flags", GS_PLUGIN_REFINE_FLAGS_REQUIRE_LICENSE |
+						 GS_PLUGIN_REFINE_FLAGS_REQUIRE_SIZE |
+						 GS_PLUGIN_REFINE_FLAGS_REQUIRE_VERSION |
+						 GS_PLUGIN_REFINE_FLAGS_REQUIRE_HISTORY |
+						 GS_PLUGIN_REFINE_FLAGS_REQUIRE_ORIGIN_HOSTNAME |
+						 GS_PLUGIN_REFINE_FLAGS_REQUIRE_MENU_PATH |
+						 GS_PLUGIN_REFINE_FLAGS_REQUIRE_URL |
+						 GS_PLUGIN_REFINE_FLAGS_REQUIRE_SETUP_ACTION |
+						 GS_PLUGIN_REFINE_FLAGS_REQUIRE_PROVENANCE |
+						 GS_PLUGIN_REFINE_FLAGS_REQUIRE_RELATED |
+						 GS_PLUGIN_REFINE_FLAGS_REQUIRE_RUNTIME |
+						 GS_PLUGIN_REFINE_FLAGS_REQUIRE_PERMISSIONS |
+						 GS_PLUGIN_REFINE_FLAGS_REQUIRE_SCREENSHOTS,
+						 NULL);
+		gs_plugin_loader_job_process_async (priv->plugin_loader, plugin_job,
+						    priv->cancellable,
+						    gs_shell_file_to_app_cb,
+						    shell);
+		return;
+	}
 	gs_app_set_local_file (app, file);
 	gs_shell_change_mode (shell, GS_SHELL_MODE_DETAILS,
-			      (gpointer) app, TRUE);
-	gs_shell_activate (shell);
+			      app, TRUE);
 }
 
 void
@@ -2156,6 +2280,26 @@ side_filter_select_by_mode (GsShell *shell, GsShellMode mode)
 					   side_filter_row_selected, shell);
 
 	gs_shell_side_filter_set_visible (shell, (row != NULL));
+}
+
+static GsCategory *
+side_filter_get_category_by_name (GsShell *shell, const gchar *category_id)
+{
+	GsShellPrivate *priv = gs_shell_get_instance_private (shell);
+	g_autoptr(GList) rows = gtk_container_get_children (GTK_CONTAINER (priv->side_filter));
+
+	for (GList *l = rows; l != NULL; l = l->next) {
+		GsCategory *category;
+		GsSideFilterRow *current = GS_SIDE_FILTER_ROW (l->data);
+
+		if (gs_side_filter_row_get_mode (current) != GS_SHELL_MODE_CATEGORY)
+			continue;
+
+		category = gs_side_filter_row_get_category (current);
+		if (g_strcmp0 (gs_category_get_id (category), category_id) == 0)
+			return category;
+	}
+	return NULL;
 }
 
 GtkWidget *

plugins/flatpak/gs-flatpak.c
@@ -3117,6 +3117,65 @@ gs_flatpak_create_runtime_repo (GsFlatpak *self,
 	return g_steal_pointer (&app);
 }
 
+GsApp *
+gs_flatpak_create_app_from_repo_dir (GsFlatpak *self,
+				     GFile *file,
+				     GCancellable *cancellable,
+				     GError **error)
+{
+	g_autoptr(GPtrArray) remotes = NULL;
+	gboolean dir_has_repo = FALSE;
+	gboolean reload_overview = FALSE;
+	GsApp *app = NULL;
+	const FlatpakRemoteType usb_type[] = { FLATPAK_REMOTE_TYPE_USB };
+
+	remotes = flatpak_installation_list_remotes_by_type (self->installation,
+							     usb_type,
+							     G_N_ELEMENTS (usb_type),
+							     cancellable, error);
+	if (remotes == NULL)
+		return NULL;
+
+	for (guint i = 0; i < remotes->len; ++i) {
+		FlatpakRemote *remote = g_ptr_array_index (remotes, i);
+		g_autofree gchar *remote_uri = flatpak_remote_get_url (remote);
+		g_autoptr(GFile) remote_file = g_file_new_for_uri (remote_uri);
+		g_autoptr(GError) local_error = NULL;
+
+		/* skip if the remote does not have the dir's path as a parent */
+		if (!g_file_has_prefix (remote_file, file))
+			continue;
+
+		dir_has_repo = TRUE;
+
+		if (!gs_flatpak_mark_apps_from_usb_remote (self, remote, &reload_overview,
+							   cancellable, &local_error)) {
+			g_debug ("Failed to mark USB apps from remote at %s: %s",
+				 remote_uri, local_error->message);
+			continue;
+		}
+	}
+
+	if (!dir_has_repo) {
+		g_autofree gchar *file_uri = g_file_get_uri (file);
+		g_set_error (error, G_IO_ERROR_FAILED, G_IO_ERROR_NOT_FOUND,
+			     "No remotes from the given directory: %s", file_uri);
+		return NULL;
+	}
+
+	app = gs_flatpak_app_new ("com.endlessm.RemovableMediaRepo");
+	gs_app_set_kind (app, AS_APP_KIND_SOURCE);
+	gs_app_set_state (app, AS_APP_STATE_INSTALLED);
+	gs_app_add_quirk (app, AS_APP_QUIRK_NOT_LAUNCHABLE);
+	gs_app_set_name (app, GS_APP_QUALITY_NORMAL, "Removable Media Repo");
+	gs_app_set_management_plugin (app, gs_plugin_get_name (self->plugin));
+	gs_app_set_metadata (app, "EndlessOS::RemovableMediaCategory", "usb");
+	if (reload_overview)
+		gs_app_set_metadata (app, "EndlessOS::ReloadOverview", "true");
+
+	return app;
+}
+
 static gboolean
 app_has_local_source (GsApp *app)
 {

plugins/flatpak/gs-flatpak.h
@@ -143,6 +143,11 @@ gboolean	gs_flatpak_add_recent		(GsFlatpak		*self,
 						 guint64		 age,
 						 GCancellable		*cancellable,
 						 GError			**error);
+GsApp *
+gs_flatpak_create_app_from_repo_dir (GsFlatpak *self,
+				     GFile *file,
+				     GCancellable *cancellable,
+				     GError **error);
 
 G_END_DECLS
 

plugins/flatpak/gs-plugin-flatpak.c
@@ -30,6 +30,7 @@
 #include <config.h>
 
 #include <flatpak.h>
+#include <glib/gi18n.h>
 #include <gnome-software.h>
 
 #include "gs-appstream.h"
@@ -414,6 +415,17 @@ gs_plugin_update_app (GsPlugin *plugin,
 	return gs_flatpak_update_app (flatpak, app, cancellable, error);
 }
 
+static gchar *
+get_dir_mount_point_name (GFile *dir,
+			  GCancellable *cancellable,
+			  GError **error)
+{
+	g_autoptr(GMount) mount = g_file_find_enclosing_mount (dir, cancellable, error);
+	if (mount == NULL)
+		return NULL;
+	return g_mount_get_name (mount);
+}
+
 static gboolean
 gs_plugin_flatpak_file_to_app_repo (GsPlugin *plugin,
 				    GsAppList *list,
@@ -424,6 +436,56 @@ gs_plugin_flatpak_file_to_app_repo (GsPlugin *plugin,
 	GsPluginData *priv = gs_plugin_get_data (plugin);
 	g_autoptr(GsApp) app_tmp = NULL;
 	g_autoptr(GsAppList) list_tmp = NULL;
+	GFileType file_type = G_FILE_TYPE_UNKNOWN;
+
+	file_type = g_file_query_file_type (file, G_FILE_QUERY_INFO_NONE, cancellable);
+
+	/* check if this is rather a directory */
+	if (file_type == G_FILE_TYPE_DIRECTORY) {
+		for (guint i = 0; i < priv->flatpaks->len; ++i) {
+			g_autoptr(GError) local_error = NULL;
+			GsFlatpak *flatpak = g_ptr_array_index (priv->flatpaks, i);
+			app_tmp = gs_flatpak_create_app_from_repo_dir (flatpak, file, cancellable,
+								       &local_error);
+			if (app_tmp != NULL)
+				break;
+
+			/* if the error is "not found" than it's just that
+			 * no remotes in this Flatpak installation matched
+			 * others in the USB remote, but other installations
+			 * can still have them... */
+			if (g_error_matches (local_error, G_IO_ERROR,
+					     G_IO_ERROR_NOT_FOUND))
+				continue;
+
+			gs_flatpak_error_convert (&local_error);
+			g_propagate_error (error, local_error);
+			return FALSE;
+		}
+
+		if (app_tmp == NULL) {
+			g_autoptr(GError) local_error = NULL;
+			g_autofree gchar *mount_name = get_dir_mount_point_name (file,
+										 cancellable,
+										 &local_error);
+			if (mount_name == NULL) {
+				mount_name = g_file_get_path (file);
+				g_debug ("Failed to get mount for %s: %s", mount_name,
+					 local_error->message);
+			}
+			/* TRANSLATORS: error message with the name of the USB
+			 * mount point or path, to inform the user we failed
+			 * to load apps from that location */
+			g_set_error (error, GS_PLUGIN_ERROR,
+				     GS_PLUGIN_ERROR_INVALID_FORMAT,
+				     _("No sources of applications found in the USB \"%s\""),
+				     mount_name);
+			return FALSE;
+		}
+
+		gs_app_list_add (list, app_tmp);
+		return TRUE;
+	}
 
 	/* parse the repo file */
 	app_tmp = gs_flatpak_app_new_from_repo_file (file, cancellable, error);
@@ -644,6 +706,7 @@ gs_plugin_file_to_app (GsPlugin *plugin,
 		NULL };
 	const gchar *mimetypes_repo[] = {
 		"application/vnd.flatpak.repo",
+		"inode/directory",
 		NULL };
 	const gchar *mimetypes_ref[] = {
 		"application/vnd.flatpak.ref",

src/gs-overview-page.c
@@ -79,6 +79,7 @@ G_DEFINE_TYPE_WITH_PRIVATE (GsOverviewPage, gs_overview_page, GS_TYPE_PAGE)
 
 enum {
 	SIGNAL_REFRESHED,
+	SIGNAL_CATEGORIES_LOADED,
 	SIGNAL_LAST
 };
 
@@ -499,6 +500,8 @@ out:
 	gs_shell_side_filter_set_visible (priv->shell, TRUE);
 
 	priv->loading_categories = FALSE;
+
+	g_signal_emit (self, signals[SIGNAL_CATEGORIES_LOADED], 0);
 
 	gs_overview_page_decrement_action_cnt (self);
 }
@@ -1154,6 +1157,13 @@ gs_overview_page_class_init (GsOverviewPageClass *klass)
 		g_signal_new ("refreshed",
 			      G_TYPE_FROM_CLASS (object_class), G_SIGNAL_RUN_LAST,
 			      G_STRUCT_OFFSET (GsOverviewPageClass, refreshed),
+			      NULL, NULL, g_cclosure_marshal_VOID__VOID,
+			      G_TYPE_NONE, 0);
+
+	signals [SIGNAL_CATEGORIES_LOADED] =
+		g_signal_new ("categories-loaded",
+			      G_TYPE_FROM_CLASS (object_class), G_SIGNAL_RUN_LAST,
+			      G_STRUCT_OFFSET (GsOverviewPageClass, categories_loaded),
 			      NULL, NULL, g_cclosure_marshal_VOID__VOID,
 			      G_TYPE_NONE, 0);
 

src/gs-overview-page.h
@@ -34,7 +34,8 @@ struct _GsOverviewPageClass
 {
 	GsPageClass		 parent_class;
 
-	void	(*refreshed)	(GsOverviewPage *self);
+	void	(*refreshed)		(GsOverviewPage *self);
+	void	(*categories_loaded)	(GsOverviewPage *self);
 };
 
 GsOverviewPage	*gs_overview_page_new		(void);

commit 23977508fd0bd43750e3d60144bc1fdeb55d9656

Restore window activation while showing a local file
Fallout from commit 54863ceaf54cf5128f19b7f6e4338905a1b155d0

https://phabricator.endlessm.com/T23365

src/gs-shell.c
@@ -2141,6 +2141,7 @@ gs_shell_show_local_file (GsShell *shell, GFile *file)
 	gs_app_set_local_file (app, file);
 	gs_shell_change_mode (shell, GS_SHELL_MODE_DETAILS,
 			      app, TRUE);
+	gs_shell_activate (shell);
 }
 
 void