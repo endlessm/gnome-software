45b08259ba65a7bb920e5aa74b0d5d408307b58a
Joaquim Rocha <jrocha@endlessm.com>
2019-02-26 13:25:07 +0100

    flatpak: Refine proxy apps
    These changes make the Flatpak plugin refine proxy apps in order to
    replace their related apps that may be "wildcards" (matching any app)
    by full apps known to the plugin.
    
    Currently this is not happening in EOS since the proxy apps that the
    EOS plugin produces are always coming from the updates list, but that
    will change soon when we include any apps needed to be installed if
    missing. So this changes shouldn't impact the list of related apps
    for now.
    
    https://phabricator.endlessm.com/T25495

--- plugins/flatpak/gs-flatpak.c
+++ plugins/flatpak/gs-flatpak.c
@@ -2577,6 +2577,43 @@ gs_flatpak_refine_appstream (GsFlatpak *self, GsApp *app, GError **error)
 	return TRUE;
 }
 
+static void
+gs_flatpak_refine_proxy_app (GsFlatpak	*self,
+			     GsApp	*app)
+{
+	GPtrArray *proxied_apps = gs_app_get_related (app);
+	g_autoptr(GsAppList) real_related_apps = NULL;
+
+	if (proxied_apps->len == 0)
+		return;
+
+	real_related_apps = gs_app_list_new ();
+
+	/* get a list of the real apps (not eventual wildcard ones) */
+	for (guint i = 0; i < proxied_apps->len; ++i) {
+		GsApp *proxied_app = g_ptr_array_index (proxied_apps, i);
+		GsApp *app_cached = proxied_app;
+
+		if (gs_app_has_quirk (proxied_app, GS_APP_QUIRK_IS_WILDCARD) &&
+		    gs_app_get_bundle_kind (proxied_app) == AS_BUNDLE_KIND_FLATPAK) {
+			const gchar *id = gs_app_get_unique_id (proxied_app);
+			app_cached = gs_plugin_cache_lookup (self->plugin, id);
+			if (app_cached == NULL)
+				continue;
+		}
+
+		gs_app_list_add (real_related_apps, app_cached);
+	}
+
+	/* set the cached apps as the related ones instead */
+	gs_app_clear_related (app);
+
+	for (guint i = 0; i < gs_app_list_length (real_related_apps); ++i) {
+		GsApp *related_app = gs_app_list_index (real_related_apps, i);
+		gs_app_add_related (app, related_app);
+	}
+}
+
 gboolean
 gs_flatpak_refine_app (GsFlatpak *self,
 		       GsApp *app,
@@ -2593,6 +2630,11 @@ gs_flatpak_refine_app (GsFlatpak *self,
 				  gs_flatpak_get_id (self),
 				  gs_app_get_unique_id (app));
 	g_assert (ptask != NULL);
+
+	if (gs_app_has_quirk (app, GS_APP_QUIRK_IS_PROXY)) {
+		gs_flatpak_refine_proxy_app (self, app);
+		return TRUE;
+	}
 
 	/* always do AppStream properties */
 	if (!gs_flatpak_refine_appstream (self, app, error))
