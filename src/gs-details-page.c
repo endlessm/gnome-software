fd2272164e9c57f85c31117ada6f97082a68542e
Philip Withnall <withnall@endlessm.com>
2019-06-27 18:43:44 +0100

    ui: Rename ‘Install’ to ‘Download’
    We need to show more directly to users that apps needs to be downloaded,
    so we're changing the installation button's label to "Download".
    
    Signed-off-by: Philip Withnall <withnall@endlessm.com>
    
    (Rebase 3.36: Fix minor rebase conflicts. Drop changes to
    `gs-upgrade-banner` as they were affected by 8b8f38fad798.)
    
    https://phabricator.endlessm.com/T21227

--- src/gs-details-page.c
+++ src/gs-details-page.c
@@ -953,7 +953,7 @@ gs_details_page_refresh_buttons (GsDetailsPage *self)
 		gtk_widget_set_visible (self->button_install, TRUE);
 		/* TRANSLATORS: button text in the header when an application
 		 * can be installed */
-		gtk_button_set_label (GTK_BUTTON (self->button_install), _("_Install"));
+		gtk_button_set_label (GTK_BUTTON (self->button_install), _("_Download"));
 		break;
 	case AS_APP_STATE_INSTALLING:
 		gtk_widget_set_visible (self->button_install, FALSE);
@@ -970,7 +970,7 @@ gs_details_page_refresh_buttons (GsDetailsPage *self)
 			gtk_widget_set_visible (self->button_install, TRUE);
 			/* TRANSLATORS: button text in the header when firmware
 			 * can be live-installed */
-			gtk_button_set_label (GTK_BUTTON (self->button_install), _("_Install"));
+			gtk_button_set_label (GTK_BUTTON (self->button_install), _("_Download"));
 		} else {
 			gtk_widget_set_visible (self->button_install, FALSE);
 		}
@@ -984,7 +984,7 @@ gs_details_page_refresh_buttons (GsDetailsPage *self)
 			 * be installed.
 			 * The ellipsis indicates that further steps are required,
 			 * e.g. enabling software repositories or the like */
-			gtk_button_set_label (GTK_BUTTON (self->button_install), _("_Install…"));
+			gtk_button_set_label (GTK_BUTTON (self->button_install), _("_Download…"));
 		}
 		break;
 	default:
