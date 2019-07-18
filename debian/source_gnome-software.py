801868f4c1a75a6c5da118d5e3d9312f9064e0b9
Philip Withnall <withnall@endlessm.com>
2019-07-18 17:47:05 +0100

    debian: Update to upstream Debian packaging for 3.31.90-1
    This is the upstream packaging, unmodified. The next commit will
    introduce Endless-specific changes.
    
    Signed-off-by: Philip Withnall <withnall@endlessm.com>
    
    https://phabricator.endlessm.com/T25367

--- debian/source_gnome-software.py
+++ debian/source_gnome-software.py
@@ -3,5 +3,4 @@ import apport.packaging
 def add_info(report, ui):
     report["InstalledPlugins"] = apport.hookutils.package_versions(
         'gnome-software-plugin-flatpak',
-        'gnome-software-plugin-limba',
         'gnome-software-plugin-snap')
