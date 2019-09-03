endlessm/gnome-software

commit 6e8cd654d209d5481a9d547b467760585fc7af72

Set the side filter categories from the overview

src/gs-overview-page.c
@@ -532,6 +532,54 @@ out:
 	gs_overview_page_decrement_action_cnt (self);
 }
 
+/*
+ * get_categories_cb:
+ * This function is used instead of the gs_shell_get_categories_cb to set the
+ * categories in the side filter. The original function is not removed so it is
+ * easier to maintain the source code.
+ */
+static void
+get_categories_cb (GObject *source_object,
+		   GAsyncResult *res,
+		   gpointer data)
+{
+	GsOverviewPage *self = GS_OVERVIEW_PAGE (data);
+	GsOverviewPagePrivate *priv = gs_overview_page_get_instance_private (self);
+	GsPluginLoader *plugin_loader = GS_PLUGIN_LOADER (source_object);
+	GsCategory *cat;
+	gboolean has_category = FALSE;
+	g_autoptr(GError) error = NULL;
+	g_autoptr(GPtrArray) list = NULL;
+
+	list = gs_plugin_loader_job_get_categories_finish (plugin_loader, res, &error);
+	if (list != NULL) {
+		gs_shell_side_filter_clear_categories (priv->shell);
+
+		for (guint i = 0; i < list->len; i++) {
+			cat = GS_CATEGORY (g_ptr_array_index (list, i));
+			if (gs_category_get_size (cat) == 0)
+				continue;
+			has_category = TRUE;
+			gs_shell_side_filter_add_category (priv->shell, cat);
+		}
+	} else {
+		if (!g_error_matches (error, GS_PLUGIN_ERROR, GS_PLUGIN_ERROR_CANCELLED))
+			g_warning ("failed to get categories: %s", error->message);
+	}
+
+	priv->empty = !has_category;
+
+	/* We always show the side filter in the overview page because it
+	 * has the "Featured" row. Re-check the mode because it's possible
+	 * to switch mode before the categories have loaded. */
+	if (gs_shell_get_mode (priv->shell) == GS_SHELL_MODE_OVERVIEW)
+		gs_shell_side_filter_set_visible (priv->shell, TRUE);
+
+	priv->loading_categories = FALSE;
+
+	gs_overview_page_decrement_action_cnt (self);
+}
+
 static void
 category_tile_clicked (GsCategoryTile *tile, gpointer data)
 {
@@ -779,6 +827,39 @@ gs_overview_page_load (GsOverviewPage *self)
 
 	priv->empty = TRUE;
 
+	if (!priv->loading_popular) {
+		g_autoptr(GsPluginJob) plugin_job = NULL;
+
+		priv->loading_popular = TRUE;
+		plugin_job = gs_plugin_job_newv (GS_PLUGIN_ACTION_GET_POPULAR,
+						 "refine-flags", GS_PLUGIN_REFINE_FLAGS_REQUIRE_RATING |
+								 GS_PLUGIN_REFINE_FLAGS_REQUIRE_ICON |
+								 GS_PLUGIN_REFINE_FLAGS_REQUIRE_ORIGIN_HOSTNAME,
+						 NULL);
+		gs_plugin_loader_job_process_async (priv->plugin_loader,
+						    plugin_job,
+						    priv->cancellable,
+						    gs_overview_page_get_popular_cb,
+						    self);
+		priv->action_cnt++;
+	}
+
+	if (!priv->loading_categories) {
+		g_autoptr(GsPluginJob) plugin_job = NULL;
+		priv->loading_categories = TRUE;
+		plugin_job = gs_plugin_job_newv (GS_PLUGIN_ACTION_GET_CATEGORIES,
+						 NULL);
+		gs_plugin_loader_job_get_categories_async (priv->plugin_loader, plugin_job,
+							   priv->cancellable,
+							   get_categories_cb,
+							   self);
+		priv->action_cnt++;
+	}
+
+	/* stop here because we don't want to load anything else but we want to
+	 * keep the code around as it makes future rebases easier */
+	return;
+
 	if (!priv->loading_featured) {
 		g_autoptr(GsPluginJob) plugin_job = NULL;
 
@@ -942,6 +1023,11 @@ gs_overview_page_switch_to (GsPage *page, gboolean scroll_up)
 	}
 
 	gs_grab_focus_when_mapped (priv->scrolledwindow_overview);
+
+	/* hide the category related UI because it is handled in the
+	 * side filter */
+	gtk_widget_set_visible (priv->categories_expander_box, FALSE);
+	gtk_widget_set_visible (priv->category_heading, FALSE);
 
 	if (priv->cache_valid || priv->action_cnt > 0)
 		return;

commit f6cc6ea18059decbf683b1bd7e1cadb376dffecc

gs-overview-page: Fix the USB category in the sidebar
This was a rebase failure caused by 6e8cd654d2 not deleting the old code
(so changes rebased into that code weren’t noticed as part of the
rebase due to not causing conflicts).

When this is next rebased, commit 6e8cd654d2 should be modified to drop
the unused code, and this commit squashed into subsequent commits.

Signed-off-by: Philip Withnall <withnall@endlessm.com>

https://phabricator.endlessm.com/T26770

src/gs-overview-page.c
@@ -573,9 +573,23 @@ get_categories_cb (GObject *source_object,
 		gs_shell_side_filter_clear_categories (priv->shell);
 
 		for (guint i = 0; i < list->len; i++) {
+			gboolean usb_category_inserted = FALSE;
 			cat = GS_CATEGORY (g_ptr_array_index (list, i));
-			if (gs_category_get_size (cat) == 0)
+
+			if (g_strcmp0 (gs_category_get_id (cat), "usb") == 0) {
+				g_autoptr(GPtrArray) copy_dests = gs_plugin_loader_dup_copy_dests (plugin_loader);
+
+				if (copy_dests != NULL && copy_dests->len > 0)
+					usb_category_inserted = TRUE;
+			}
+
+			/* allow empty categories for USB since there are usable
+			 * actions (such as copy OS to USB) in the USB category
+			 * even when it has no apps available
+			 */
+			if (gs_category_get_size (cat) == 0 && !usb_category_inserted)
 				continue;
+
 			has_category = TRUE;
 			gs_shell_side_filter_add_category (priv->shell, cat);
 		}
@@ -593,6 +607,9 @@ get_categories_cb (GObject *source_object,
 		gs_shell_side_filter_set_visible (priv->shell, TRUE);
 
 	priv->loading_categories = FALSE;
+
+	if (list != NULL)
+		g_signal_emit (self, signals[SIGNAL_CATEGORIES_LOADED], 0);
 
 	gs_overview_page_decrement_action_cnt (self);
 }