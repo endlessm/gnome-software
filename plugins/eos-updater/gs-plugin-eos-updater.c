254f4fffbbe073703ce8e0b550ad85823b8b225b
Philip Withnall <pwithnall@endlessos.org>
2022-11-29 13:55:36 +0000

    eos-updater: Clear download size once update is fetched
    The `GsApp:download-size` property is the size of *what remains* to be
    downloaded, not the size of the entire update. That means it needs to be
    updated to zero once the update is complete.
    
    This fixes the ‘Download’ button sometimes being shown again after
    downloading the update. In particular, this can happen if gnome-software
    is started when the eos-updater daemon is part-way through an update
    (and hence the gnome-software process has not proceeded through the Poll
    phase).
    
    Signed-off-by: Philip Withnall <pwithnall@endlessos.org>
    Upstream: https://gitlab.gnome.org/GNOME/gnome-software/-/merge_requests/1555
      Tweaked to drop size validity and use `priv`
    
    Fixes: #1985
    
    https://phabricator.endlessm.com/T34184

--- plugins/eos-updater/gs-plugin-eos-updater.c
+++ plugins/eos-updater/gs-plugin-eos-updater.c
@@ -414,12 +414,17 @@ sync_state_from_updater_unlocked (GsPlugin *plugin)
 	}
 	case EOS_UPDATER_STATE_UPDATE_READY: {
 		app_set_state (plugin, app, AS_APP_STATE_UPDATABLE);
+
+		/* Nothing further to download. */
+		gs_app_set_size_download (app, 0);
+
 		break;
 	}
 	case EOS_UPDATER_STATE_APPLYING_UPDATE: {
 		/* set as 'installing' because if it is applying the update, we
 		 * want to show the progress bar */
 		app_set_state (plugin, app, AS_APP_STATE_INSTALLING);
+		gs_app_set_size_download (app, 0);
 
 		/* set up the fake progress to inform the user that something
 		 * is still being done (we don't get progress reports from
@@ -436,6 +441,7 @@ sync_state_from_updater_unlocked (GsPlugin *plugin)
 	}
 	case EOS_UPDATER_STATE_UPDATE_APPLIED: {
 		app_set_state (plugin, app, AS_APP_STATE_UPDATABLE);
+		gs_app_set_size_download (app, 0);
 
 		break;
 	}
@@ -913,6 +919,9 @@ gs_plugin_app_upgrade_download (GsPlugin *plugin,
 			/* if there's an update ready to deployed, and it was started by
 			 * the user, we should proceed to applying the upgrade */
 			gs_app_set_progress (app, max_progress_for_update);
+
+			/* Nothing further to download. */
+			gs_app_set_size_download (app, 0);
 
 			if (!gs_eos_updater_call_apply_sync (priv->updater_proxy,
 							     cancellable, error)) {
